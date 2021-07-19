f70ead9ef932aecd86f60b98f769fa84b68505c1
Jordan Ball <cjordanball@ballwebdev.com>
2021-07-19 19:56:55 -0400

    create app

---
+++ My BTC/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  My BTC
+//
+//  Created by C. Jordan Ball III on 7/19/21.
+//
+
+import Foundation
+
+struct Constants {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate";
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
---
+++ My BTC/Private.swift
@@ -0,0 +1,12 @@
+//
+//  Private.swift
+//  My BTC
+//
+//  Created by C. Jordan Ball III on 7/19/21.
+//
+
+import Foundation
+
+struct Private {
+    let apiKey = "9E4C694C-683F-43CB-92FA-040DDA6421D7";
+}
